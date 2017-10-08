54da4bee4044948134502792969f24e5af7234a3
= <=>
2017-10-08 00:31:06 -0400

    Add character controller and colliders

--- Bobblehead-War/Assets/Scripts/PlayerController.cs
+++ Bobblehead-War/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public float movingSpeed = 50.0f;
+    private CharacterController characterController;
+
 	// Use this for initialization
 	void Start () {
-		
+        characterController = this.GetComponent<CharacterController>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 position = this.transform.position;
-        position.x += movingSpeed * Input.GetAxis("Horizontal") * Time.deltaTime;
-        position.z += movingSpeed * Input.GetAxis("Vertical") * Time.deltaTime;
-        this.transform.position = position;
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * movingSpeed);
 	}
 }
