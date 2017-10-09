ef5bdcd1a65052ed7aa8153c96f2b5115b5411ea
= <=>
2017-10-08 22:49:17 -0400

    adding destroy method and layers

---
+++ Bobblehead-War/Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+
+    //use this to destry the object when it cannot be seen by any camera
+    private void OnBecameInvisible() {
+        Destroy(this.gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        Destroy(this.gameObject);
+    }
+}
